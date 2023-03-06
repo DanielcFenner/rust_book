f9e85a80d2f48e4b72fa3ec27009104f1bee1879
Daniel Fenner <danielfennerdev@gmail.com>
2023-03-05 20:56:37 -0500

    cargo knowledge obtained

---
+++ hello_cargo/src/main.rs
@@ -0,0 +1,8 @@
+// This was created with "cargo new hello_cargo" in the terminal
+// We can use "cargo build" to build it and "cargo run" to build and run
+// "cargo check" checks that the file will compile without creating an executable. It is faster than cargo run.
+// To build for release use "cargo build --release"
+
+fn main() {
+    println!("Hello, world!");
+}
--- hello_world/main.rs
+++ hello_world/main.rs
@@ -1,3 +1,13 @@
+// Main is the entry point to every rust program
+// Rust indents with 4 spaces
+
 fn main() {
+    // ! means we are calling a macro, not a function
     println!("Hello, world!");
 }
+
+// To compile this in the terminal:
+// rustc main.rs
+
+// And to run it:
+// ./main

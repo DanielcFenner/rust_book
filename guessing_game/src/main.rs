36118a9f238eab3c62ac461d249609fb34ba52d5
Daniel Fenner <danielfennerdev@gmail.com>
2023-03-06 18:40:49 -0500

    guessing game completed

--- guessing_game/src/main.rs
+++ guessing_game/src/main.rs
@@ -17,35 +17,42 @@ fn main() {
     // thread_rng is local to the current thread of execution
     let secret_number = rand::thread_rng().gen_range(1..=100);
 
-    println!("The secret number is: {secret_number}");
+    //println!("The secret number is: {secret_number}");
+    loop {
+        println!("Please input your guess.");
 
-    println!("Please input your guess.");
+        // Variables in rust are immutable by default (cant be changed)
+        // So we have to state mut to make them mutable
+        // String::new() returns a new instance of String which is growable UTF-8 encoded bit of text
+        // ::new indicates that new is an associated function of String
+        let mut guess = String::new();
 
-    // Variables in rust are immutable by default (cant be changed)
-    // So we have to state mut to make them mutable
-    // String::new() returns a new instance of String which is growable UTF-8 encoded bit of text
-    // ::new indicates that new is an associated function of String
-    let mut guess = String::new();
+        // stdin() is a function of io and .read_line is a method of that
+        // & makes guess a reference, and we have to declare it mutable
+        // .expect handles an Err return from read_line and crashes the program
 
-    // stdin() is a function of io and .read_line is a method of that
-    // & makes guess a reference, and we have to declare it mutable
-    // .expect handles an Err return from read_line and crashes the program
+        io::stdin()
+            .read_line(&mut guess)
+            .expect("failed to read line");
 
-    io::stdin()
-        .read_line(&mut guess)
-        .expect("failed to read line");
+        // we parse guess to an int so we can compare it with the
+        // secret_number. trim removes whitespace
+        let guess: u32 = match guess.trim().parse() {
+            Ok(num) => num,
+            Err(_) => continue,
+        };
 
-    // we parse guess to an int so we can compare it with the
-    // secret_number. trim removes whitespace
-    let guess: u32 = guess.trim().parse().expect("Please type a number!");
+        println!("You guessed: {guess}");
 
-    println!("You guessed: {guess}");
-
-    // cmp takes a reference to whatever you want to compare to
-    match guess.cmp(&secret_number) {
-        Ordering::Less => println!("Too small!"),
-        Ordering::Greater => println!("Too big!"),
-        Ordering::Equal => println!("You win!"),
+        // cmp takes a reference to whatever you want to compare to
+        // breaks the loop if win
+        match guess.cmp(&secret_number) {
+            Ordering::Less => println!("Too small!"),
+            Ordering::Greater => println!("Too big!"),
+            Ordering::Equal => {
+                println!("You win!");
+                break;
+            },
+        }
     }
-
 }
